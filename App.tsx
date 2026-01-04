d7263338fd467c191584aa7a848fe303b0481acb
faiqmisbah <faiqmisbah019@gmail.com>
2026-01-04 15:58:00 +0700

    fix: remove min-h-screen to fix footer overflow issue

--- App.tsx
+++ App.tsx
@@ -8,7 +8,7 @@ import Footer from './components/Footer';
 
 const App: React.FC = () => {
   return (
-    <div className="min-h-screen bg-background-dark text-white font-sans selection:bg-primary selection:text-white overflow-x-hidden">
+    <div className="bg-background-dark text-white font-sans selection:bg-primary selection:text-white overflow-x-hidden">
       <Navbar />
       <main className="flex flex-col items-center w-full">
         <Hero />
